0af7b4f6223ea91eb351b4db7916469a11be7257
Joe Man <joe.man@local.com>
2015-08-20 23:41:42 +0800

    init

---
+++ www/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://hr.local/admin/');
+define('HTTP_CATALOG', 'http://hr.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://hr.local/admin/');
+define('HTTPS_CATALOG', 'http://hr.local/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\www\vizz\HR\www/admin/');
+define('DIR_SYSTEM', 'D:\www\vizz\HR\www/system/');
+define('DIR_LANGUAGE', 'D:\www\vizz\HR\www/admin/language/');
+define('DIR_TEMPLATE', 'D:\www\vizz\HR\www/admin/view/template/');
+define('DIR_CONFIG', 'D:\www\vizz\HR\www/system/config/');
+define('DIR_IMAGE', 'D:\www\vizz\HR\www/image/');
+define('DIR_CACHE', 'D:\www\vizz\HR\www/system/cache/');
+define('DIR_DOWNLOAD', 'D:\www\vizz\HR\www/system/download/');
+define('DIR_UPLOAD', 'D:\www\vizz\HR\www/system/upload/');
+define('DIR_LOGS', 'D:\www\vizz\HR\www/system/logs/');
+define('DIR_MODIFICATION', 'D:\www\vizz\HR\www/system/modification/');
+define('DIR_CATALOG', 'D:\www\vizz\HR\www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'leave_record');
+define('DB_PREFIX', 'oc_');
---
+++ www/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://hr.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://hr.local/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\www\vizz\HR\www/catalog/');
+define('DIR_SYSTEM', 'D:\www\vizz\HR\www/system/');
+define('DIR_LANGUAGE', 'D:\www\vizz\HR\www/catalog/language/');
+define('DIR_TEMPLATE', 'D:\www\vizz\HR\www/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\www\vizz\HR\www/system/config/');
+define('DIR_IMAGE', 'D:\www\vizz\HR\www/image/');
+define('DIR_CACHE', 'D:\www\vizz\HR\www/system/cache/');
+define('DIR_DOWNLOAD', 'D:\www\vizz\HR\www/system/download/');
+define('DIR_UPLOAD', 'D:\www\vizz\HR\www/system/upload/');
+define('DIR_MODIFICATION', 'D:\www\vizz\HR\www/system/modification/');
+define('DIR_LOGS', 'D:\www\vizz\HR\www/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'leave_record');
+define('DB_PREFIX', 'oc_');
